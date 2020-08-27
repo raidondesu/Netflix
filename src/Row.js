a47f57a2e37ee3e89e794ea840692a0062bf75b6
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-08-27 19:31:56 -0400

    Managing state in Row Componenet

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Row({ title }) {
+    const [movies, setMovies] = useState([]);
     return (
         <div>
             <h2>(title)</h2>
