8cc4d31625e774a92dd8eb958f9c059513cdc7ba
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-08-27 19:00:48 -0400

    FetchTrending, FetchOriginals, FetchTopRated
    Trending, Originals, and top rated endpoints

--- src/request.js
+++ src/request.js
@@ -1,1 +1,7 @@
 const APIKEY =  "93bbe9101a48aa8ae2d09bbe427896f1";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`
+}
