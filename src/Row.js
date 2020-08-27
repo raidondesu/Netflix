50fc02a461b35bce98fb7b1c6bae573e22485e8d
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-08-27 19:26:08 -0400

    Building out row component

---
+++ src/Row.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Row({ title }) {
+    return (
+        <div>
+            <h2>(title)</h2>
+
+            {/* Container -> Posters */}
+        </div>
+    )
+}
+
+export default Row
