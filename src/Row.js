12416429fbf7bc93e3f689fbb4960dc7483b1032
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-08-28 23:01:59 -0400

    Asynchronous requests for rows

--- src/Row.js
+++ src/Row.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import axios from './axios';
 
 function Row({ title }) {
     const [movies, setMovies] = useState([]);
@@ -6,6 +7,11 @@ function Row({ title }) {
     //When the row loads, make a requests to the database
     useEffect(() => {
         // if [] is blank, run once when the row loads and dont run again
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            //BaseURL + Row URL + APIKEY
+        }
+        fetchData();
     }, []);
     return (
         <div>
