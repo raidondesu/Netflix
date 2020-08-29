13ac1b82eafbb37c7c1027d7a7d749717bd459d7
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-08-28 21:54:38 -0400

    Pseudo code for Useeffect
    Determines what happens when a row is loaded

--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Row({ title }) {
     const [movies, setMovies] = useState([]);
+
+    //When the row loads, make a requests to the database
+    useEffect(() => {
+        // if [] is blank, run once when the row loads and dont run again
+    }, []);
     return (
         <div>
             <h2>(title)</h2>
