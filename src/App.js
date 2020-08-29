c15ea95d4fe40eb55fc10130adbd0d43a2f58a8f
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-08-28 21:49:53 -0400

    Fetch requests

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import requests from './requests';
 function App() {
   return (
     <div className="App">
-      <Row title = "Netflix Originals" fetchUrl={}/>
-      <Row title = "Trending Now" fetchUrl={}/>
-      <Row title = "Top Rated" fetchUrl={}/>
-      <Row title = "Action Movies" fetchUrl={}/>
-      <Row title = "Comedy Movies" fetchUrl={}/>
-      <Row title = "Romance Movies" fetchUrl={}/>
+      <Row title = "Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title = "Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title = "Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title = "Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title = "Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title = "Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
     </div>
   );
 }
