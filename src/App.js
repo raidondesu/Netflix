30821b68952bda35739f4ac19ca042b91db26165
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-08-28 21:45:41 -0400

    Template for fetching requests

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
-
+import Row from './Row';
+import requests from './requests';
 function App() {
   return (
     <div className="App">
-      <Row title = "Netflix Originals"/>
-      <Row title = "Trending Now"/>
-      <Row title = "Top Rated"/>
-      <Row title = "Action Movies"/>
-      <Row title = "Comedy Movies"/>
-      <Row title = "Romance Movies"/>
+      <Row title = "Netflix Originals" fetchUrl={}/>
+      <Row title = "Trending Now" fetchUrl={}/>
+      <Row title = "Top Rated" fetchUrl={}/>
+      <Row title = "Action Movies" fetchUrl={}/>
+      <Row title = "Comedy Movies" fetchUrl={}/>
+      <Row title = "Romance Movies" fetchUrl={}/>
     </div>
   );
 }
