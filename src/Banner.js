1c6b8c5b09ab01402996444a3bd89efbb24785b4
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-08-30 12:13:16 -0400

    Banner Content without styling

--- src/Banner.js
+++ src/Banner.js
@@ -33,9 +33,10 @@ function Banner() {
         <h1>{movie?.title || movie?.name || movie?.originalname}</h1>
         {/* div 2 buttons */}
         <div className="banner_buttons">
-          <button className="banner_button"></button>
-          <button className="banner_button"></button>
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
         </div>
+        <h1 className="banner_description">{movie?.overview}</h1>
         {/* description */}
       </div>
     </header>
