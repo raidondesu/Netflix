d030b8aa0a7b4f83860b98f69c2edce008f0b751
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-08-30 14:45:16 -0400

    Nav avatar and netflix logo fixed to the stop
    When we scroll the navbar stays at the top

--- src/Nav.js
+++ src/Nav.js
@@ -8,6 +8,11 @@ function Nav() {
         src="https://cdn.iconscout.com/icon/free/png-128/netflix-282224.png"
         alt="Netflix Logo"
       />
+      <img
+        className="nav_avatar"
+        src="https://img.icons8.com/clouds/2x/netflix.png"
+        alt="Netflix Logo"
+      />
     </div>
   );
 }
