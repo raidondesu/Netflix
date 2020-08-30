569ebe4678403c17d79e1c1a15697bb780c9e4ef
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-08-30 15:03:39 -0400

    React hook to manage state for our eventlistener handleShow

--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Nav.css";
 function Nav() {
+  const [show, handleShow] = useState(false);
   useEffect(() => {
     window.addEventListener("scroll", () => {
       if (window.scrollY > 100) {
         handleShow(true);
       } else handleShow(false);
     });
+    return () => {
+      window.removeEventListener("scroll");
+    };
   }, []);
   return (
     <div className="nav">
