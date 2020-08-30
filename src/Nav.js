de91b2551a6c728476a353bb75653bf128ac4e62
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-08-30 14:50:54 -0400

    Styling navbar (will add avatar later)

--- src/Nav.js
+++ src/Nav.js
@@ -1,16 +1,11 @@
 import React from "react";
-
+import "./Nav.css";
 function Nav() {
   return (
     <div className="nav">
       <img
         className="nav_logo"
         src="https://cdn.iconscout.com/icon/free/png-128/netflix-282224.png"
-        alt="Netflix Logo"
-      />
-      <img
-        className="nav_avatar"
-        src="https://img.icons8.com/clouds/2x/netflix.png"
         alt="Netflix Logo"
       />
     </div>
