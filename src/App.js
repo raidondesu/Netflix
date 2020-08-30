d220620ba800d9d6a72b448f4da7ef1df175565a
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-08-29 23:51:01 -0400

    Created Row CSS for styling
     - Added prettier code plugin to format code on save

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.css';
-import Row from './Row';
-import requests from './request';
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./request";
 function App() {
   return (
     <div className="App">
-      <Row title ="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row title ="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,35 +1,39 @@
-import React, { useState, useEffect } from 'react'
-import axios from './axios';
-
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
 const poster_url = "https://image.tmdb.org/t/p/original";
 
 function Row({ title, fetchUrl }) {
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-    //When the row loads, make a requests to the database
-    useEffect(() => {
-        // if [] is blank, run once when the row loads and dont run again
-        async function fetchData() {
-            const request = await axios.get(fetchUrl);
-            //BaseURL + Row URL + APIKEY
-            // request.data.results is the array that contains the movies for each row
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
-    }, [fetchUrl]);
+  //When the row loads, make a requests to the database
+  useEffect(() => {
+    // if [] is blank, run once when the row loads and dont run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      //BaseURL + Row URL + APIKEY
+      // request.data.results is the array that contains the movies for each row
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-    console.table(movies);
-    return (
-        <div className = "row">
-            <h2>{title}</h2>
-            <div className = "row_posters">
-                {movies.map(movie => (
-                    <img src = {`${poster_url}${movie.poster_path}`} alt={movie.name}/>
-                ))}
-            </div>
-        </div>
-    )
+  console.table(movies);
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            className="row_poster"
+            src={`${poster_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Row
+export default Row;
